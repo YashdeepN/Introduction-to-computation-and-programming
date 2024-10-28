50593392392ac6dac6d1dab0aec0f3fe5e09c2c3
YashdeepN <yashdeepneema@hotmail.com>
2024-10-28 20:16:34 +0530

    01 finger exercise

---
+++ 02-Introduction-to-python/01_finger_exercise.py
@@ -0,0 +1,33 @@
+x = int(input("x = "))
+y = int(input("y = "))
+z = int(input("z = "))
+
+if x > y and x > z:
+    first_number = x
+elif y > z:
+    first_number = y
+else: 
+    first_number = z
+
+if first_number == x:
+    if y > z:
+        second_number, third_number = y, z
+    else:
+        second_number, third_number = z, y
+elif first_number == y:
+    if x > z:
+        second_number, third_number = x, z
+    else:
+        second_number, third_number = z, x
+else:
+    if x > y:
+        second_number, third_number = x, y
+    else:
+        second_number, third_number = y, x
+
+if first_number % 2 != 0:
+    print(first_number)
+elif second_number % 2 != 0:
+    print(second_number)
+else:
+    print(third_number)
