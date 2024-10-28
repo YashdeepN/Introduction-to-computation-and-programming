180040a7188b5b063df0e873e46b463dd6f1a638
YashdeepN <yashdeepneema@hotmail.com>
2024-10-28 20:23:46 +0530

    01 finger exercise make elegent

--- 02-Introduction-to-python/01_finger_exercise.py
+++ 02-Introduction-to-python/01_finger_exercise.py
@@ -2,6 +2,7 @@ x = int(input("x = "))
 y = int(input("y = "))
 z = int(input("z = "))
 
+"""
 if x > y and x > z:
     first_number = x
 elif y > z:
@@ -30,4 +31,14 @@ if first_number % 2 != 0:
 elif second_number % 2 != 0:
     print(second_number)
 else:
-    print(third_number)
+    print(third_number) """
+
+
+answer = min(x, y, z)
+if x % 2 != 0:
+    answer = x
+if y > answer and y % 2 != 0:
+    answer = y
+if z > answer and z % 2 != 0:
+    answer = z
+print(answer)
